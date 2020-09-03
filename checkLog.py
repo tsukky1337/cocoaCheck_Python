ef0328aa7e045a36c1e2c90422326164702b28a7
kageponex <tsukky1337@gmail.com>
2020-09-03 12:30:42 +0900

    Create checkLog.py

---
+++ checkLog.py
@@ -0,0 +1,36 @@
+# import libs
+import pathlib,glob,json
+
+# function def
+def printLog(data):
+	if (int(data['MatchCount']) == 0):
+		return
+		
+	str = ''
+	str = str + data['Hash'] + '\n'
+	str = str + ' '
+	str = str + data['Timestamp'] + '\n'
+	
+	print(str)
+
+def doCheck(logFilePath):
+	with open('./' + logFilePath) as jsonFile:
+		jsonObj = json.load(jsonFile)
+		version = int(jsonObj['ExportVersion'])
+		datas = jsonObj['ExposureChecks']
+	
+		if version == 1:
+			for data in datas:
+				printLog(data)
+					
+		elif version == 2:
+			for data in datas:
+				for file in data['Files']:
+					printLog(file)
+
+#main
+path = pathlib.Path('./')
+logList = list(path.glob('ExposureChecks-*.json'))
+
+for log in logList:
+	doCheck(log.name)
